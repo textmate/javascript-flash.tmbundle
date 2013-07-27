a14ce952840c3a996089673e64b1f62339f38a4b
Michael Sheets <git@whitefalls.org>
2013-07-27 03:11:52 -0500

    Use version 1.8 of ruby for bundle items
    10.7 and 10.8 only include ruby 1.8, so all bundle items have been written to work with that. Optionally supporting ruby 1.9 and 2.0 is problematic as these versions are not fully backwards compatible.
    
    #ignore

--- Support/squeeze.sh
+++ Support/squeeze.sh
@@ -4,7 +4,7 @@ SQUEEZER="/Applications/TextMate.app/Contents/SharedSupport/Bundles/Objective-C.
 
 cd data
 
-ruby $SQUEEZER < objects.txt > objects_compressed.txt
-ruby $SQUEEZER < methods.txt > methods_compressed.txt
-ruby $SQUEEZER < properties.txt > properties_compressed.txt
+/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < objects.txt > objects_compressed.txt
+/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < methods.txt > methods_compressed.txt
+/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < properties.txt > properties_compressed.txt
 
