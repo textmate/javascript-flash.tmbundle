7d96a2ebd63e4ded55630ec4b83c2227b345a616
Michael Sheets <git@whitefalls.org>
2014-06-18 03:57:24 -0500

    Point to ruby 1.8 shim
    Using a shim allows us to catch when 1.8 of ruby is not present and provide other options.
    
    #ignore

--- Support/squeeze.sh
+++ Support/squeeze.sh
@@ -4,7 +4,7 @@ SQUEEZER="/Applications/TextMate.app/Contents/SharedSupport/Bundles/Objective-C.
 
 cd data
 
-/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < objects.txt > objects_compressed.txt
-/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < methods.txt > methods_compressed.txt
-/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby $SQUEEZER < properties.txt > properties_compressed.txt
+ruby18 $SQUEEZER < objects.txt > objects_compressed.txt
+ruby18 $SQUEEZER < methods.txt > methods_compressed.txt
+ruby18 $SQUEEZER < properties.txt > properties_compressed.txt
 
